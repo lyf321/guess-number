820b89909963c71933bedddc321b361bf5b00218
李杨帆 <2531349593@qq.com>
2016-07-26 18:01:56 +0800

    AnswerGenerator and test

--- main/models/AnswerGenerator.js
+++ main/models/AnswerGenerator.js
@@ -1,3 +1,16 @@
-/**
- * Created by liyangfan on 16-7-26.
- */
+class AnswerGenerator{
+    
+   static generate(){
+       const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+       const result = [];
+            for(var i = 0; i < 4 ; i ++) {
+                var id = parseInt(Math.random()*numbers.length);
+                result.push((numbers.splice(id,1))[0]);
+            }
+
+       return result;
+    }
+}
+
+module.exports = AnswerGenerator;
--- spec/answerGenerator-spec.js
+++ spec/answerGenerator-spec.js
@@ -1,3 +1,16 @@
-/**
- * Created by liyangfan on 16-7-26.
- */
+'use strict'
+const AnswerGenerator = require('../main/models/AnswerGenerator');
+
+describe('answer Generator', () => {
+
+    it('should generator answer', () => {
+        const isUnique = (item,index,array)=>{
+            return array.lastIndexOf(item)===index;
+        };
+        const answer = AnswerGenerator.generate();
+
+        expect(answer.length).toEqual(4);
+        expect(answer.every(isUnique)).toBeTruthy;
+        expect(AnswerGenerator.generate()).not.toEqual(AnswerGenerator.generate());
+    });
+});
