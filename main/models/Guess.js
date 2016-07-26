2367d69d964a22d668ff11a2f975143574aa34bd
李杨帆 <2531349593@qq.com>
2016-07-26 18:05:58 +0800

    Guess class and test

--- main/models/Guess.js
+++ main/models/Guess.js
@@ -1,3 +1,13 @@
-/**
- * Created by liyangfan on 16-7-26.
- */
+const AnswerGenerator = require('./AnswerGenerator');
+const CompareNumber = require('./CompareNumber');
+
+class Guess {
+
+    static guess(input) {
+        const answer = AnswerGenerator.generate();
+        const compareNumber = CompareNumber.guessNumber(input,answer);
+        return compareNumber;
+    }
+}
+
+module.exports = Guess;
--- spec/guess-spec.js
+++ spec/guess-spec.js
@@ -1,3 +1,13 @@
-/**
- * Created by liyangfan on 16-7-26.
- */
+const Guess = require('../main/models/Guess');
+const AnswerGenerator = require('../main/models/AnswerGenerator');
+
+
+describe('Guess', () => {
+
+    it('should print correct XAXB',() => {
+        spyOn(AnswerGenerator,'generate').and.returnValue('1234');
+        const result = Guess.guess('1234');
+        expect(result).toEqual('4A0B')
+    })
+    
+})
